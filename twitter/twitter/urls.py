15e3807f5e7f5f891f9b1175ddad295a3a433cd3
dumel93 <damian.krawczyk.9393@gmail.com>
2017-07-07 15:38:46 +0200

    template and model tweet added

--- twitter/twitter/urls.py
+++ twitter/twitter/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from twitter_comments import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^twitter/', views.TwitterBaseView.as_view(), name="base"),
     url(r'^twitter_users/', include('twitter_users.urls')),
     url(r'^twitter_entries/', include('twitter_entries.urls')),
     url(r'^twitter_comments/', include('twitter_comments.urls')),
     url(r'^twitter_messages/', include('twitter_messages.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
