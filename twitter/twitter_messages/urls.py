1bc42bdbeb486ae493553291ea456ab5a01eaeb3
mikbhub <mikolaj.babiak@gmail.com>
2017-07-07 17:43:08 +0200

    created views and templates for messages

--- twitter/twitter_messages/urls.py
+++ twitter/twitter_messages/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-
+from twitter_messages import views
 
 app_name = 'twitter_messages'
 
 urlpatterns = [
-    
+    url(r'^message_add/', views.MessageCreateView.as_view(), name='message-add'),
+    url(r'^message_list/', views.MessageListView.as_view(), name="message-list" ),
+    url(r'^message_detail/(?P<pk>(\d)+)', views.MessageDetailView.as_view(), name="message-detail" ),
 ]
