5fb7848d1394028215c2a739b5241d0f0d332887
mikbhub <mikolaj.babiak@gmail.com>
2017-07-07 17:16:20 +0200

    added model Message

--- twitter/twitter_messages/models.py
+++ twitter/twitter_messages/models.py
@@ -3,3 +3,30 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Message(models.Model):
+    """Model definition for Message."""
+
+    # TODO: Define fields here
+    text = models.TextField(max_length=666)
+    sender = models.ForeignKey(to=User, related_name='messages_sent')
+    receiver = models.ForeignKey(to=User, related_name='messages_received')
+    was_read = models.BooleanField(default=False)
+
+    class Meta:
+        """Meta definition for Message."""
+
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+    
+    @property
+    def message_text_short(self):
+        # show first 20 characters of a message
+        return self.text[:20]
+    
+    def __str__(self):
+        return 'text: {text} from: {sender}, to: {receiver}, read: {was_read}'.format(
+            text=self.message_text_short,
+            sender=self.sender.username,
+            receiver=self.receiver.username,
+            was_read=self.was_read,
+        )
--- twitter/twitter_messages/urls.py
+++ twitter/twitter_messages/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
 
+app_name = 'twitter_messages'
+
 urlpatterns = [
     
-] 
- 
+]
